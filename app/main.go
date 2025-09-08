3426654b07abd0a39508706b996ca14b5560e18c
Abhishek Mandingi <mabhi256@gmail.com>
2025-09-08 16:27:40 -0400

    #SH9 (Combining Character Classes)

--- app/main.go
+++ app/main.go
@@ -1,32 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"os"
-	"slices"
-	"unicode/utf8"
 )
-
-func isDigit(char byte) bool {
-	return char >= '0' && char <= '9'
-}
-
-func hasDigit(line []byte) bool {
-	return slices.ContainsFunc(line, isDigit)
-}
-
-func isAlphaNumeric(char byte) bool {
-	isSmall := char >= 'a' && char <= 'z'
-	isCapitalized := char >= 'A' && char <= 'Z'
-
-	return isSmall || isCapitalized || isDigit(char) || char == '_'
-}
-
-func hasAlphaNumeric(line []byte) bool {
-	return slices.ContainsFunc(line, isAlphaNumeric)
-}
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 func main() {
@@ -57,49 +35,13 @@ func main() {
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
-	var ok bool
+	tokens := tokenize(pattern)
 
-	n := len(pattern)
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
-
-	switch {
-	case pattern == "\\d":
-		ok = hasDigit(line)
-
-	case pattern == "\\w":
-		ok = hasAlphaNumeric(line)
-
-	case n >= 3 && pattern[0] == '[' && pattern[1] != '^' && pattern[n-1] == ']':
-		for _, char := range line {
-			for _, check := range []byte(pattern[1 : n-1]) {
-				if char == check {
-					return true, nil
-				}
-			}
+	for i := range len(line) {
+		if matchTokensAt(tokens, line, i) {
+			return true, nil
 		}
-
-	case n >= 4 && pattern[0] == '[' && pattern[1] == '^' && pattern[n-1] == ']':
-		for _, char := range line {
-			var found bool
-			for _, check := range []byte(pattern[2 : n-1]) {
-				if char == check {
-					found = true
-					break
-				}
-			}
-
-			if !found {
-				return true, nil
-			}
-		}
-
-	case utf8.RuneCountInString(pattern) == 1:
-		ok = bytes.ContainsAny(line, pattern)
-
-	default:
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
 
-	return ok, nil
+	return false, nil
 }
---
+++ app/tokenizer.go
@@ -0,0 +1,48 @@
+package main
+
+func tokenize(pattern string) []string {
+	var tokens []string
+
+	i := 0
+	for i < len(pattern) {
+		ch := pattern[i]
+
+		switch ch {
+		case '[':
+			token, pos := readCharClass(pattern, i)
+			tokens = append(tokens, token)
+			i = pos
+
+		case '\\':
+			token, pos := readEscape(pattern, i)
+			tokens = append(tokens, token)
+			i = pos
+
+		default:
+			tokens = append(tokens, string(pattern[i]))
+			i++
+		}
+	}
+
+	return tokens
+}
+
+func readCharClass(pattern string, startPos int) (string, int) {
+	charClass := ""
+	for i := startPos; i < len(pattern); i++ {
+		charClass += string(pattern[i])
+		if pattern[i] == ']' {
+			return charClass, i + 1
+		}
+	}
+
+	return "", len(pattern)
+}
+
+func readEscape(pattern string, startPos int) (string, int) {
+	if len(pattern) == startPos+1 {
+		return "", len(pattern)
+	}
+
+	return pattern[startPos : startPos+2], startPos + 2
+}
